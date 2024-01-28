c99e9852e1bdb6944a93c6dfce6eecc4a00176b3
Fdvalls <fernandodanielvalls@gmail.com>
2024-01-28 01:18:06 -0300

    v1

---
+++ ejercicios_practica/marvel/e_commerce/api/views.py
@@ -0,0 +1,158 @@
+from django.forms.models import model_to_dict
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
+import json
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.validators import ValidationError
+
+from e_commerce.models import Comic
+
+# @api_view(http_method_names=["GET"])
+# def comic_retrieve_api_view(request):
+#     if request.method == 'GET':
+#         instance = Comic.objects.filter(id=request.GET.get('id')).first()
+#         if instance:
+#             return JsonResponse(data=model_to_dict(instance), status=200)
+#         return JsonResponse(data={}, status=404)
+#     else:
+#         return JsonResponse(
+#             data={"message": "Método HTTP no permitido."},
+#             status=405
+#         )
+
+@api_view(http_method_names=["GET"])
+def comic_list_filtered_api_view(request):
+    filtered_comics = Comic.objects.filter(price__gt=5.00)
+    _data = list(filtered_comics.values()) if filtered_comics.exists() else []
+    return Response(data=_data, status=status.HTTP_200_OK)
+
+# def comic_list_api_view(request):
+#     if request.method == 'GET':
+#         _queryset = Comic.objects.all()
+#         _data = list(_queryset.values()) if _queryset.exists() else []
+#         return JsonResponse(data=_data, safe=False, status=200)
+#     else:
+#         return JsonResponse(
+#             data={"message": "Método HTTP no permitido."},
+#             status=405
+#         )
+
+# @csrf_exempt
+# def comic_create_api_view(request):
+#     if request.method == 'POST':
+#         Casteo el query-dict request.POST a un dict para poder aplicar
+#         método .pop() ya que el query-dict es inmutable.
+#         _request = dict(request.POST)
+#         _marvel_id = _request.pop('marvel_id', None)
+#         if not _marvel_id:
+#             return JsonResponse(
+#                 data={"marvel_id": "Este campo no puede ser nulo."},
+#                 status=400
+#             )
+#         _instance, _created = Comic.objects.get_or_create(
+#             marvel_id=_marvel_id,
+#             defaults=_request.POST
+#         )
+#         if _created:
+#             return JsonResponse(
+#                 data=model_to_dict(_instance), status=201
+#             )
+#         return JsonResponse(
+#             data={
+#                 "marvel_id": "Ya existe un comic con ese valor, debe ser único."
+#             },
+#             status=400
+#         )
+#     else:
+#         return JsonResponse(
+#             data={"message": "Método HTTP no permitido."},
+#             status=405
+#         )
+
+@csrf_exempt
+def comic_create_api_view(request):
+    if request.method == 'POST':
+        try:
+            # Intenta obtener datos JSON desde el cuerpo de la solicitud
+            data = json.loads(request.body.decode('utf-8'))
+
+            # Imprime el cuerpo de la solicitud
+            print(f"Cuerpo de la solicitud: {data}")
+
+            # Extrae el campo marvel_id de los datos
+            _marvel_id = data.get('marvel_id', None)
+            print(f"que tengo en _marvel_id: {_marvel_id}")
+
+            # Verifica si marvel_id es nulo
+            if _marvel_id is None:
+                raise ValueError("marvel_id no puede ser nulo.")
+
+            # Resto de tu código...
+            _instance, _created = Comic.objects.get_or_create(
+                marvel_id=_marvel_id,
+                defaults=data  # Utiliza directamente los datos obtenidos
+            )
+
+            if _created:
+                return JsonResponse(
+                    data=model_to_dict(_instance),
+                    status=201
+                )
+            else:
+                return JsonResponse(
+                    data={"marvel_id": "Ya existe un comic con ese valor, debe ser único."},
+                    status=400
+                )
+
+        except ValueError as e:
+            return JsonResponse(data={"error": str(e)}, status=400)
+
+    else:
+        return JsonResponse(
+            data={"message": "Método HTTP no permitido."},
+            status=405
+        )
+
+# NOTE: Ahora comenzamos usando DRF:
+@api_view(http_method_names=['GET'])
+def comic_list_api_view(request):
+    _queryset = Comic.objects.all()
+    _data = list(_queryset.values()) if _queryset.exists() else []
+    return Response(data=_data, status=status.HTTP_200_OK)
+
+
+@api_view(http_method_names=['GET'])
+def comic_retrieve_api_view(request):
+    instance = get_object_or_404(
+        Comic, id=request.query_params.get('id')
+    )
+    return Response(
+        data=model_to_dict(instance), status=status.HTTP_200_OK
+    )
+
+
+# @api_view(http_method_names=['POST'])
+# def comic_create_api_view(request):
+#     _marvel_id = request.data.pop('marvel_id', None)
+#     print(request.data)
+#     if not _marvel_id:
+#         raise ValidationError(
+#             {"marvel_id": "Este campo no puede ser nulo."}
+#         )
+#     _instance, _created = Comic.objects.get_or_create(
+#         marvel_id=_marvel_id,
+#         defaults=request.data
+#     )
+#     if _created:
+#         return Response(
+#             data=model_to_dict(_instance), status=status.HTTP_201_CREATED
+#         )
+#     return Response(
+#         data={
+#             "marvel_id": "Ya existe un comic con ese valor, debe ser único."
+#         },
+#         status=status.HTTP_400_BAD_REQUEST
+#     )
